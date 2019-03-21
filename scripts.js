42ab856bad89446fe509eabf0afa785b8bb4677d
Chris Schuck <chris@rosieapp.com>
2019-03-21 11:12:52 -0400

    Cleans up some logic in anticipation of moving over to Vue and Firestore

--- scripts.js
+++ scripts.js
@@ -1,172 +1,128 @@
-//var imageGroup = $(".imageGroup");
-//var imageGroup = document.getElementsByClassName("imageGroup");
-//var circleRight = $("#circleRight").find("div:nth-of-type(2)");
-var circleRight = document.getElementById('arrowRight');
-//var rightArrow = circleRight.querySelectorAll("div:nth-of-type(2)");
-//var circleLeft = $("#circleLeft");
-var circleLeft = document.getElementById("circleLeft");
-var i = 0;
-//var imageGroupArray = $(".imageGroup");
-var imageGroupArray = document.getElementsByClassName("imageGroup");
-//var getTransitionDelay = document.getElementById("circleLeft").currentStyle.display;
-var getTransitionDelay = getStyle(imageGroupArray[0], "transition-duration");
-getTransitionDelay.replace(/\D/g, '');
-var transitionDelay = parseFloat(getTransitionDelay) * 1000;
-//var imageGroupImage2 = $(".imageGroup").find("img:nth-of-type(2)");
-var imageGroupImage2 = document.querySelectorAll(".imageGroup img:nth-of-type(2)");
-//var imageGroupImage3 = $(".imageGroup").find("img:nth-of-type(3)");
-var imageGroupImage3 = document.querySelectorAll(".imageGroup img:nth-of-type(3)");
-var countClass = $(".count");
-var countLastAfter = $('#countLastAfter');
-var title = $(".totals-list__anchor");
-var overallTotal = 0;
+$(document).ready(function () {
+	var circleRight = document.getElementById('arrowRight');
+	var circleLeft = document.getElementById("circleLeft");
+	var i = 0;
+	var imageGroupArray = document.getElementsByClassName("imageGroup");
+	var getTransitionDelay = getStyle(imageGroupArray[0], "transition-duration");
+	getTransitionDelay.replace(/\D/g, '');
+	var transitionDelay = parseFloat(getTransitionDelay) * 1000;
+	var imageGroupImage2 = document.querySelectorAll(".imageGroup img:nth-of-type(2)");
+	var imageGroupImage3 = document.querySelectorAll(".imageGroup img:nth-of-type(3)");
+	var countClass = $(".count");
+	var countLastAfter = $('#countLastAfter');
+	var title = $(".totals-list__anchor");
+	var overallTotal = 0;
 
-function changeImagesUp(x, y) {
-    if (imageGroupArray[x].offsetWidth > 2) {
-        imageGroupArray[x].classList.remove("newImageGroupClass2");
-        imageGroupArray[x].classList.add("newImageGroupClass");
-        imageGroupArray[y].classList.add("newImageGroupClass2");
-        imageGroupImage2[x].style.webkitTransform = 'translateX(100px)';
-        imageGroupImage2[x].style.transform = 'translateX(100px)';
-        imageGroupImage3[x].style.webkitTransform = 'translateX(150px)';
-        imageGroupImage3[x].style.transform = 'translateX(150px)';
-        imageGroupImage2[y].style.webkitTransform = 'translateX(0)';
-        imageGroupImage2[y].style.transform = 'translateX(0)';
-        imageGroupImage3[y].style.webkitTransform = 'translateX(-50px)';
-        imageGroupImage3[y].style.transform = 'translateX(-50px)';
-    }
-}
-circleRight.addEventListener('click', clickEvent);
+	function changeImagesUp(x, y) {
+		if (imageGroupArray[x].offsetWidth > 2) {
+			imageGroupArray[x].classList.remove("newImageGroupClass2");
+			imageGroupArray[x].classList.add("newImageGroupClass");
+			imageGroupArray[y].classList.add("newImageGroupClass2");
+			imageGroupImage2[x].style.webkitTransform = 'translateX(100px)';
+			imageGroupImage2[x].style.transform = 'translateX(100px)';
+			imageGroupImage3[x].style.webkitTransform = 'translateX(150px)';
+			imageGroupImage3[x].style.transform = 'translateX(150px)';
+			imageGroupImage2[y].style.webkitTransform = 'translateX(0)';
+			imageGroupImage2[y].style.transform = 'translateX(0)';
+			imageGroupImage3[y].style.webkitTransform = 'translateX(-50px)';
+			imageGroupImage3[y].style.transform = 'translateX(-50px)';
+		}
+	}
+	circleRight.addEventListener('click', clickEvent);
 
-function imageMove() {
-    circleLeft.removeAttribute('disabled');
-    for (i = 0, len = imageGroupArray.length; i < len; i++) {
-        changeImagesUp(i, i + 1);
-    }
-    if (imageGroupArray[imageGroupArray.length - 2].offsetWidth > 2) {
-        circleRight.setAttribute('disabled', '');
-        circleLeft.focus();
-    }
-    setTimeout(function() {
-        circleRight.addEventListener('click', clickEvent);
-    }, transitionDelay);
-}
+	function imageMove() {
+		circleLeft.removeAttribute('disabled');
+		for (i = 0, len = imageGroupArray.length; i < len; i++) {
+			changeImagesUp(i, i + 1);
+		}
+		if (imageGroupArray[imageGroupArray.length - 2].offsetWidth > 2) {
+			circleRight.setAttribute('disabled', '');
+			circleLeft.focus();
+		}
+		setTimeout(function () {
+			circleRight.addEventListener('click', clickEvent);
+		}, transitionDelay);
+	}
 
-function clickEvent() {
-    circleRight.removeEventListener("click", clickEvent);
-    imageMove();
-    return false;
-}
-circleLeft.addEventListener("click", clickEvent2)
+	function clickEvent() {
+		circleRight.removeEventListener("click", clickEvent);
+		imageMove();
+		return false;
+	}
+	circleLeft.addEventListener("click", clickEvent2)
 
-function imageMove2() {
-    circleRight.removeAttribute('disabled');
-    for (i = 0, len = imageGroupArray.length; i < len; i++) {
-        changeImagesUp(i, i - 1);
-    }
-    if (imageGroupArray[1].offsetWidth > 2) {
-        circleLeft.setAttribute('disabled', '');
-        circleRight.focus();
-    }
-    setTimeout(function() {
-        circleLeft.addEventListener("click", clickEvent2)
-    }, transitionDelay);
-}
+	function imageMove2() {
+		circleRight.removeAttribute('disabled');
+		for (i = 0, len = imageGroupArray.length; i < len; i++) {
+			changeImagesUp(i, i - 1);
+		}
+		if (imageGroupArray[1].offsetWidth > 2) {
+			circleLeft.setAttribute('disabled', '');
+			circleRight.focus();
+		}
+		setTimeout(function () {
+			circleLeft.addEventListener("click", clickEvent2)
+		}, transitionDelay);
+	}
 
-function clickEvent2() {
-    circleLeft.removeEventListener("click", clickEvent2);
-    imageMove2();
-    return false;
-}
-//for getting the style from the existing external style sheet
-function getStyle(oElm, strCssRule) {
-    var strValue = "";
-    if (document.defaultView && document.defaultView.getComputedStyle) {
-        strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
-    } else if (oElm.currentStyle) {
-        strCssRule = strCssRule.replace(/\-(\w)/g, function(strMatch, p1) {
-            return p1.toUpperCase();
-        });
-        strValue = oElm.currentStyle[strCssRule];
-    }
-    return strValue;
-}
+	function clickEvent2() {
+		circleLeft.removeEventListener("click", clickEvent2);
+		imageMove2();
+		return false;
+	}
+	//for getting the style from the existing external style sheet
+	function getStyle(oElm, strCssRule) {
+		var strValue = "";
+		if (document.defaultView && document.defaultView.getComputedStyle) {
+			strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
+		} else if (oElm.currentStyle) {
+			strCssRule = strCssRule.replace(/\-(\w)/g, function (strMatch, p1) {
+				return p1.toUpperCase();
+			});
+			strValue = oElm.currentStyle[strCssRule];
+		}
+		return strValue;
+	}
 
-$(function() {
-    var primaryNav = $("#primary-nav")
-    $("#menu-button").on("click",function(){
-        $(this).toggleClass("menu-is-open");
-        primaryNav.on("webkitTransitionEnd transitionend").toggleClass("primary-nav--is-visible")
-    });
-    $.each(countClass, function(i, item) {
-        var dataString = $(item).attr('data-value').toString();
-        var theCompleteString = dataString;
-        var urlsAll = [theCompleteString];
-        var totalAll = 0;
-        $.ajaxSetup({
-            timeout: 8000
-        });
-        $.get(urlsAll).done(function(data) {
-                totalAll += $(data).find(".yearGroup").find("li").length - $(data).find(".yearGroup").find(".red").length;
-
-                //var random = Math.round(Math.random()* totalAll);
-                //console.log(type);
-                //Don't include the cards that I don't own
-                //var randomness = hidden.find('li').not('li:has(.red)').eq(random).text();
-                //console.log(randomness);
-                var totalForTitle = $(title[i]).attr('data-value');
-                var totalForEndTitle = $(title).last().attr('data-value');
-                var formatIndividualTitle = 100 * (totalAll) / totalForTitle;
-                $(title[i]).attr("data-title", "Completion Stats: " + ReplaceNumberWithCommas(totalAll) + "/" + ReplaceNumberWithCommas(totalForTitle) + " or " + parseFloat(formatIndividualTitle.toFixed(1)) + "%" /*+ "<br /><hr />" + "Cards include: " + randomness*/ );
-                overallTotal += totalAll;
-                var formatIndividualEndTitle = 100 * (overallTotal) / totalForEndTitle;
-                $(title).last().attr("data-title", "Overall Stats: " + ReplaceNumberWithCommas(overallTotal) + "/" + ReplaceNumberWithCommas(totalForEndTitle) + " or " + parseFloat(formatIndividualEndTitle.toFixed(1)) + "%")
-                /*$(item).fadeOut(300, function() {
-                    var span = $("<span class='newSpan'>&nbsp;" + totalAll + "</span>").hide();
-                    $(item).replaceWith(span);
-
-                    span.fadeIn(400);
-                    countLast.text(ReplaceNumberWithCommas(overallTotal)).show();
-                });*/
-		var spinner = $('.spinner');
-		//spinner.fadeOut(300);
-		$(item).text(ReplaceNumberWithCommas(totalAll)).addClass('expand');
+	var primaryNav = $("#primary-nav")
+	$("#menu-button").on("click", function () {
+		$(this).toggleClass("menu-is-open");
+		primaryNav.on("webkitTransitionEnd transitionend").toggleClass("primary-nav--is-visible")
+	});
+	$.each(countClass, function (i, item) {
+		var dataString = $(item).attr('data-value').toString();
+		var theCompleteString = dataString;
+		var urlsAll = [theCompleteString];
+		var totalAll = 0;
+		$.ajaxSetup({
+			timeout: 8000
+		});
+		$.get(urlsAll).done(function (data) {
+			totalAll += $(data).find(".yearGroup").find("li").length - $(data).find(".yearGroup").find(".red").length;
+			var totalForTitle = $(title[i]).attr('data-value');
+			var totalForEndTitle = $(title).last().attr('data-value');
+			var formatIndividualTitle = 100 * (totalAll) / totalForTitle;
+			$(title[i]).attr("data-title", "Completion Stats: " + ReplaceNumberWithCommas(totalAll) + "/" + ReplaceNumberWithCommas(totalForTitle) + " or " + parseFloat(formatIndividualTitle.toFixed(1)) + "%");
+			overallTotal += totalAll;
+			var formatIndividualEndTitle = 100 * (overallTotal) / totalForEndTitle;
+			$(title).last().attr("data-title", "Overall Stats: " + ReplaceNumberWithCommas(overallTotal) + "/" + ReplaceNumberWithCommas(totalForEndTitle) + " or " + parseFloat(formatIndividualEndTitle.toFixed(1)) + "%")
+			var spinner = $('.spinner');
+			$(item).text(ReplaceNumberWithCommas(totalAll)).addClass('expand');
 			countLastAfter.text(ReplaceNumberWithCommas(overallTotal)).addClass('move-count-last-after');
-            });
-    });
+		});
+	});
 
-    function ReplaceNumberWithCommas(yourNumber) {
-        //Seperates the components of the number
-        var n = yourNumber.toString().split(".");
-        //Comma-fies the first part
-        n[0] = n[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-        //Combines the two sections
-        return n.join(".");
-    }
-    $(".arrow-up2").click(function() {
-        $('html, body').animate({
-            scrollTop: 0
-        }, 700, 'easeInOutCubic');
-    });
+	function ReplaceNumberWithCommas(yourNumber) {
+		//Seperates the components of the number
+		var n = yourNumber.toString().split(".");
+		//Comma-fies the first part
+		n[0] = n[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		//Combines the two sections
+		return n.join(".");
+	}
+	$(".arrow-up2").click(function () {
+		$('html, body').animate({
+			scrollTop: 0
+		}, 700, 'easeInOutCubic');
+	});
 });
-
-/*var mainNavigation = document.querySelectorAll("#cd-main-nav ul");
-var trigger = document.getElementById('menuButton');
-trigger.addEventListener('click', toggleMenu)
-		//$(this).toggleClass('menu-is-open');
-function toggleMenu(){
-    trigger.classList.toggle("menu-is-open");
-    trigger.getElementById("cd-main-nav").getElementsByTagName("ul").classList.toggle("is-visible");
-}*/
-/*$("#pause").click(function() {
-  timer = setInterval(function () {
-      for(i=0, len = imageGroupArray.length; i < len;i++){
-        changeImagesUp(i,i+1);
-    }
-  }, interval);
-});
-
-$("#expand").click(function() {
-  clearInterval(timer);
-  timer = null
-});*/
