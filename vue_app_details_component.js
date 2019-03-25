0182f456430fe843031e394d7e5ab67f7c2809b7
Chris Schuck <chris@rosieapp.com>
2019-03-25 13:40:32 -0400

    Updates the base, inserts, parallels page to use Vue.

--- vue_app_details_component.js
+++ vue_app_details_component.js
@@ -14,6 +14,38 @@ domReady(function () {
   firebase.initializeApp(config);
   const db = firebase.firestore();
 
+  const sharedMethods = {
+    toggleIndividual: function (event) {
+      target = event.target;
+      if (!event.target.open) {
+        $('html, body').animate({scrollTop: $(target).offset().top - 75}, 0);
+      }
+    },
+    buildCardString: function (card) {
+      let baseString = card.year + ' ' + card.set + ' #' + card.number;
+      if (card.other_info != '') {
+        baseString += ' ' + card.other_info;
+      }
+      if (card.other_players != '') {
+        baseString += ' w/' + card.other_players;
+      }
+      if (card.serial_numbered != '') {
+        baseString += ' ' + card.serial_numbered;
+      }
+      if (card.grade != '') {
+        baseString += ' ' + card.grade;
+      }
+      return baseString;
+    },
+    buildHashData: function (card) {
+      return card.year + '-' + card.set.replace(/\s+/g, '-').toLowerCase();
+    },
+    openFirstLink: function (event) {
+      event.preventDefault();
+			$('a[data-caption="' + '' + event.target.alt + '"]:first').trigger('click');
+    }
+  }
+
   Vue.component('details-component', {
     template: `
     <details class="details-year" :open="localStorage.isOpen" @toggle="toggleIndividual">
@@ -57,33 +89,57 @@ domReady(function () {
     props: {
       card: {}
     },
-    methods: {
-      toggleIndividual: function (event) {
-        target = event.target;
-        if (!event.target.open) {
-          $('html, body').animate({scrollTop: $(target).offset().top - 75}, 0);
-        }
-      },
-      buildCardString: function (card) {
-        let baseString = card.year + ' ' + card.set + ' #' + card.number;
-        if (card.other_info != '') {
-          baseString += ' ' + card.other_info;
-        }
-        if (card.other_players != '') {
-          baseString += ' w/' + card.other_players;
-        }
-        if (card.serial_numbered != '') {
-          baseString += ' ' + card.serial_numbered;
-        }
-        if (card.grade != '') {
-          baseString += ' ' + card.grade;
-        }
-        return baseString;
-      },
-      buildHashData: function (card) {
-        return card.year + '-' + card.set.replace(/\s+/g, '-').toLowerCase();
-      }
-    }
+    methods: sharedMethods
+  });
+
+  Vue.component('details-no-thumbnails-component', {
+    template: `
+    <details class="details-year" :open="localStorage.isOpen" @toggle="toggleIndividual">
+      <summary class="details-year__summary">
+        <svg role="presentation" class="details-year__toggle" xmlns="http://www.w3.org/2000/svg" height="40"
+          width="40" viewBox="0 0 40 40">
+          <rect class="details-year__toggle-border" stroke-linejoin="round" height="40" width="40"
+            stroke="#206287" stroke-linecap="round" stroke-width="4" fill="#fff"></rect>
+          <g class="details-year__toggle-plus-wrapper">
+            <rect class="details-year__toggle-vertical-line" height="25" width="4" y="7" x="18" stroke-width="0"
+              fill="#206287"></rect>
+            <rect class="details-year__toggle-horizontal-line" transform="matrix(0,-1,-1,0,0,0)" height="25"
+              width="4" y="-32" x="-22" stroke-width="0" fill="#206287"></rect>
+          </g>
+        </svg>
+        <h2 class="details-year__summary-header">{{Object.values(card)[0].total}}</h2>
+      </summary>
+      <div class="p">
+				<ol class="yearGroup numbered-list">
+          <li v-for="card in Object.values(card)[0].all_cards">
+            <span v-if="card.img_src === ''">
+              {{buildCardString(card)}}
+            </span>
+            <a v-else-if="card.img_src != '' && !Array.isArray(card.img_src)" data-fancybox="gallery" class="inserts" :data-hash="buildHashData(card)" :data-caption="buildCardString(card)" :href="card.img_src">
+              {{buildCardString(card)}}
+            </a>
+            <a v-else aria-label="Open Image in Gallery" class="inserts" data-fancybox="gallery" :data-hash="buildHashData(card)" :data-caption="buildCardString(card)" :href="card.img_src[0]">
+              {{buildCardString(card)}}
+            </a>
+          </li>
+        </ol>
+        <ul class="imgColumn">
+          <li v-for="card in Object.values(card)[0].all_cards.filter((card) => {return card.img_src != ''})">
+            <a v-if="card.img_src != '' && !Array.isArray(card.img_src)" @click="openFirstLink" aria-label="Open Image in Gallery" :data-hash="buildHashData(card)" :data-caption="buildCardString(card)" :href="card.img_src">
+              <img :data-src="card.img_src" :class="[card.img_size, 'lazyload', 'thumbnail']" :alt="buildCardString(card)">
+            </a>
+            <a v-else-if="card.img_src != '' && Array.isArray(card.img_src)" @click="openFirstLink" class="activate-gallery" aria-label="Open Image in Gallery" :data-hash="buildHashData(card)" :data-caption="buildCardString(card)" :href="card.img_src[0]">
+              <img :data-src="card.img_src[0]" :class="[card.img_size, 'lazyload', 'thumbnail']" :alt="buildCardString(card)">
+            </a>
+          </li>
+        </ul>
+      </div>
+    </details>
+    `,
+    props: {
+      card: {}
+    },
+    methods: sharedMethods
   });
 
   window.app = new Vue({
